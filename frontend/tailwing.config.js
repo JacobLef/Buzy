45375fc8092b4c2f840f4978556d0c857cb07b8a
JacobLef <jacoblefk@gmail.com>
2025-12-31 14:33:44 -0500

    Update the landing page to be more appealing to the eye and visually appetizing. Create a tailwing configuration file as well.

---
+++ frontend/tailwing.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'blob': 'blob 7s infinite',
+        'blob-delayed': 'blob 7s infinite 2s',
+        'blob-delayed-more': 'blob 7s infinite 4s',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
